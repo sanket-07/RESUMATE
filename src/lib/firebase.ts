b76900301c3446486165befe1671bcc4aa95507c
sanketgawade <22207018sanket@viva-technology.org>
2026-02-13 10:49:50 +0530

    Add GoogleAuthProvider to Firebase setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
@@ -15,3 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app); 
+
+export const googleProvider = new GoogleAuthProvider();
